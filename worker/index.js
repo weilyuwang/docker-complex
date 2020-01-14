9c4a85caec3fbc2e742d59e024c1e642decf0aa3
weilyuwang <weilyuwang@gmail.com>
2020-01-13 20:51:56 -0500

    add react client, express server and worker

---
+++ worker/index.js
@@ -0,0 +1,30 @@
+const keys = require("./keys");
+const redis = require("redis");
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+
+const sub = redisClient.duplicate();
+
+function fib(index) {
+  let a = 1;
+  let b = 0;
+  let temp = 0;
+
+  while (index >= 0) {
+    temp = a;
+    a = a + b;
+    b = temp;
+    index--;
+  }
+
+  return b;
+}
+
+sub.on("message", (channel, message) => {
+  redisClient.hset("values", message, fib(parseInt(message)));
+});
+sub.subscribe("insert");
