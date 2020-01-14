3eac6eaa9650fdfac49d0bbce37d3302bf0a60a4
weilyuwang <weilyuwang@gmail.com>
2020-01-14 13:24:35 -0500

    fixes bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import OtherPage from "./OtherPage";
@@ -9,9 +8,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-
+        <header>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
